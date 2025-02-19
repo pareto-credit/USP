5b1986352e2d6ae379a316e37b18b3ee01b1bb47
bugduino <william@idle.finance>
2025-02-19 18:01:06 +0100

    FIX: update deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,34 +2,25 @@
 pragma solidity >=0.8.28 <0.9.0;
 
 import { ParetoDollar } from "../src/ParetoDollar.sol";
-
-import { BaseScript } from "./Base.s.sol";
+import { Script } from "forge-std/src/Script.sol";
 import "forge-std/src/console.sol";
 
-contract DeployScript is BaseScript {
+contract DeployScript is Script {
   address public DEPLOYER = 0xE5Dab8208c1F4cce15883348B72086dBace3e64B;
 
   function run() public broadcast {
-    // forge script ./script/Deploy.s.sol \
-    // --fork-url $ETH_RPC_URL \
-    // --ledger \
-    // --broadcast \
-    // --optimize \
-    // --optimizer-runs 999999 \
-    // --verify \
-    // --with-gas-price 5000000000 \
-    // --sender "0xE5Dab8208c1F4cce15883348B72086dBace3e64B" \
-    // --slow \
-    // -vvv
-
     _deploy();
   }
 
-  function _deploy() public returns (
-    ParetoDollar par
-  ) {
+  function _deploy() public returns (ParetoDollar par) {
     // Deploy ParetoDollar
     par = new ParetoDollar();
     console.log('ParetoDollar deployed at:', address(par));
+  }
+
+  modifier broadcast() {
+    vm.startBroadcast();
+    _;
+    vm.stopBroadcast();
   }
 }
