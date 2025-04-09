d3ceab3cab9514845899660be88e03c713c363b6
bugduino <william@idle.finance>
2025-04-09 12:27:22 +0200

    FIX: remove console import

--- src/ParetoDollarStaking.sol
+++ src/ParetoDollarStaking.sol
@@ -7,8 +7,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
 import "./EmergencyUtils.sol";
 
-import "forge-std/console.sol";
-
 /* 
 
 ██████╗  █████╗ ██████╗ ███████╗████████╗ ██████╗     ███████╗██╗   ██╗███████╗██████╗ 
